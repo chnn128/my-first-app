db74312f39a1e50510ea0fa81d674e5a1d51c4ca
chnn128 <143911146+chnn128@users.noreply.github.com>
2023-11-13 12:10:14 -0500

    Weather app implementation (#10)
    * requirements.txt and creation
    
    creating the app and updating requirements.txt, display_forecast is showing more than forecast_demo
    
    * Weather app. display_forecast code & test file
    
    * weather app code & testing code
    
    * updating Readme File
    
    * removing domain from readme for privacy
    
    * adding additional tests to weather app
    
    * app extreme refactoring!!

--- test/weather_app_test.py
+++ test/weather_app_test.py
@@ -1,5 +1,5 @@
 from app.weather_app import display_forecast
-from app.weather_app import get_location_detail
+from app.weather_app import get_and_display_location_detail
 from app.weather_app import forecast_demo
 
 def test_display_forecast():
@@ -9,4 +9,4 @@ def test_forecast_demo():
     assert str(type(forecast_demo())) == "<class 'IPython.core.display.HTML'>"
 
 def test_get_location_detail():
-    assert str(type(get_location_detail())) == "<class 'pandas.core.series.Series'>"
+    assert str(type(get_and_display_location_detail())) == "<class 'pandas.core.series.Series'>"

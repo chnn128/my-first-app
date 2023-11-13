4189e6601944c01a0f5945dd9b50cfa4d319b6f5
chnn128 <143911146+chnn128@users.noreply.github.com>
2023-11-13 17:07:27 -0500

    updating weather app tests

--- test/weather_app_test.py
+++ test/weather_app_test.py
@@ -1,12 +1,15 @@
 from app.weather_app import display_forecast
 from app.weather_app import get_and_display_location_detail
 from app.weather_app import forecast_demo
+from pandas import Series 
+from IPython.core.display import HTML
 
 def test_display_forecast():
     assert display_forecast() == 200
 
 def test_forecast_demo():
-    assert str(type(forecast_demo())) == "<class 'IPython.core.display.HTML'>"
+    assert isinstance(forecast_demo(), HTML) == True
+    #assert str(type(forecast_demo())) == "<class 'IPython.core.display.HTML'>"
 
 def test_get_location_detail():
-    assert str(type(get_and_display_location_detail())) == "<class 'pandas.core.series.Series'>"
+    assert isinstance(get_and_display_location_detail(), Series) == True
